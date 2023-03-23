677339aa88e2005aa5f7e1e1d1887876f3b0367b
AlegkaLata <127931687+AlegkaLata@users.noreply.github.com>
2023-03-23 22:30:31 +0200

    Create  pairs.py

---
+++ pairs.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 5, 6, 7]
+
+for i inrange(len(numbers)):
+
+ for j inrange(i+1, len(numbers)):
+
+
+        if numbers[i] + numbers[j] == 10:
+
+
+            print(numbers[i], numbers[j])
